e14bea945c80d22a5b9cce334ac6ba4c51754b2d
samslow <shsvkdldj@likelion.org>
2020-04-04 05:20:45 +0900

    Resolve 딴속카메라
    Signed-off-by: samslow <shsvkdldj@likelion.org>

--- Programmers/단속카메라/solution.py
+++ Programmers/단속카메라/solution.py
@@ -1,46 +1,11 @@
-def bfs(graph, n):
-    queue, result= [], []
-    distances = {i:0 for i in range(1, n+1)}
-    
-    queue.append(graph[1])
-    
-    distance = 1
-    while queue:
-        for _ in range(len(queue)):
-            nodes = queue.pop(0)
-            for node in nodes:
-                if (distances[node] == 0):
-                    distances[node] = distance
-
-                    routes = []
-                    for edge in graph[node]:
-                        routes.append(edge)
-                    queue.append(routes)
-        distance += 1
-        
-    del distances[1]
-            
-    return distances;
-    
-
-def solution(n, edge):
-    answer = 0
-    graph = {}
-    # BFS Search를 시도
-    for e in edge:
-        if e[0] in graph:
-            graph[e[0]].append(e[1])
+def solution(routes):
+    routes = sorted(routes)
+    answer = 1
+    temp = routes.pop(0)
+    for r in routes:
+        if max(r[0], temp[0]) <= min(r[1],temp[1]):
+            temp = [max(r[0],temp[0]),min(r[1],temp[1])]
         else:
-            graph[e[0]] = [e[1]]
-        if e[1] in graph:
-            graph[e[1]].append(e[0])
-        else:
-            graph[e[1]] = [e[0]]
-    
-    distance_max = max(bfs(graph, n).values())
-    for value in bfs(graph, n).values():
-        if value == distance_max:
+            temp = r
             answer += 1
-    
-            
     return answer
